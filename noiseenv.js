aea46001aa728bad9ee5074ce2c8b5ce0b220fd0
clbmtch <113548074+clbmtch@users.noreply.github.com>
2023-04-29 13:52:52 -0500

    Add files via upload

--- noiseenv.js
+++ noiseenv.js
@@ -1,38 +1,18 @@
-﻿let noise, env, analyzer;
+﻿let song;
 
 function setup() {
-    createCanvas(710, 200);
-    noise = new p5.Noise(); // other types include 'brown' and 'pink'
-    noise.start();
-
-    // multiply noise volume by 0
-    // (keep it quiet until we're ready to make noise!)
-    noise.amp(0);
-
-    env = new p5.Env();
-    // set attackTime, decayTime, sustainRatio, releaseTime
-    env.setADSR(0.001, 0.1, 0.2, 0.1);
-    // set attackLevel, releaseLevel
-    env.setRange(1, 0);
-
-    // p5.Amplitude will analyze all sound in the sketch
-    // unless the setInput() method is used to specify an input.
-    analyzer = new p5.Amplitude();
-}
-
-function draw() {
-    background(0);
-
-    // get volume reading from the p5.Amplitude analyzer
-    let level = analyzer.getLevel();
-
-    // use level to draw a green rectangle
-    let levelHeight = map(level, 0, 0.4, 0, height);
-    fill(100, 250, 100);
-    rect(0, height, width, -levelHeight);
+    song = loadSound('souljaboy.mp3');
+    createCanvas(720, 200);
+    background(255, 0, 0);
 }
 
 function mousePressed() {
-    env.play(noise);
+    if (song.isPlaying()) {
+        // .isPlaying() returns a boolean
+        song.stop();
+        background(255, 0, 0);
+    } else {
+        song.play();
+        background(0, 255, 0);
+    }
 }
-
